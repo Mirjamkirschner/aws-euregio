b99ddfbd843a27d60bfc884cc901b70ae9c392f6
Mirjamkirschner <mirjam.kirschner@gmx.de>
2023-05-11 16:32:22 +0200

    add pointToLayer for temperatures

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ let layerControl = L.control.layers({
     "Esri WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap"),
     "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery")
 }, {
-    "Wetterstationen": themaLayer.stations.addTo(map),
+    "Wetterstationen": themaLayer.stations,
     "Temperatur": themaLayer.temperature.addTo(map),
 }).addTo(map);
 
@@ -66,10 +66,23 @@ function writeStationLayer(jsondata) {
     }).addTo(themaLayer.stations);
 }
 
+function writeTemperatureLayer(jsondata) {
+    L.geoJSON(jsondata, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                icon: L.divIcon({
+                    html: `<span>${feature.properties.LT}</span>`,
+                })
+            });
+        },
+    }).addTo(themaLayer.temperature);
+}
+
 // Vienna Sightseeing Haltestellen
 async function loadStations(url) {
     let response = await fetch(url);
     let jsondata = await response.json();
     writeStationLayer(jsondata);
+    writeTemperatureLayer(jsondata);
 }
 loadStations("https://static.avalanche.report/weather_stations/stations.geojson");
